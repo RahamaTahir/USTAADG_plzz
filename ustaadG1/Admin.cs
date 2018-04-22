b419de3133de68bd2d15f981d8d9d7a3851fad48
KhadijaZahoor <khadijazahoor77@gmail.com>
2018-04-22 15:59:19 +0500

    Domain Model

---
+++ ustaadG1/Admin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ustaadG1
+{
+    class Admin
+    {
+        private int rating;
+
+        public int Rating { get => rating; set => rating = value; }
+
+        public int UpdateRating(string feedback);
+    }
+}
