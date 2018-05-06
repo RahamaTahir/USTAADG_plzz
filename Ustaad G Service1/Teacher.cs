90de71ed38c2b9002ba129cab3902cbb8447fffe
Javeria Riaz <javeriariazmalik@gmail.com>
2018-05-06 19:30:54 +0500

    Pay and Receive revised

--- Ustaad G Service1/Teacher.cs
+++ Ustaad G Service1/Teacher.cs
@@ -25,6 +25,7 @@ namespace Ustaad_G_Service1
         private bool paid = false;
         public bool receive = false;
         private int id;
+        public int l;
 
         public string Username { get => username; set => username = value; }
         public string Age { get => age; set => age = value; }
